37f55abdd60fdfcde79f6be38932c76d336e4d47
Dragos <->
2023-09-30 09:20:26 +0300

    Final version

--- WebAPI/Models/UserModel.cs
+++ WebAPI/Models/UserModel.cs
@@ -10,7 +10,7 @@ namespace WebAPI.Models
         public string Username { get; set; }
         public string Password { get; set; }
         public string Name { get; set; }
-        public int Coins { get; set; }
+        public string Coins { get; set; }
         public DateTime CreationDate { get; set; }
     }
 }
